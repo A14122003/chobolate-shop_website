f50e6e450cc4b7ba8f8a32ab0cb511089429f0f2
Aditya1412 <korpakwadaditya@gmail.com>
2025-07-13 18:01:56 +0530

    Delete product.js

--- product.js
+++
@@ -1,8 +0,0 @@
-burger=document.querySelector('.burger')
-navbarItems=document.querySelector('.navbar')
-nav=document.querySelector('.items')
-
-burger.addEventListener('click',()=>{
-    navbarItems.classList.toggle('h-class')
-    nav.classList.toggle('v-class')
-})
